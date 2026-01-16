c63695eadd926db26e6f836a309a7f61295c56da
Lemmy <lemmy_paul.schreiber@mailbox.tu-dresden.de>
2026-01-16 12:41:50 +0100

    funktionierende einlese und Normalieseierung von txt datein sowohl im WX Format als auch im .W Format

---
+++ KartendatenNormaliesierne.py
@@ -0,0 +1,38 @@
+from DateiEinlesen import Labyrinth, LabListeZeilen, LabWall, LabHeight, LabLength
+LabNormal = []
+StartKordX = 0
+StartKordY = 0
+ZielKordX = 0
+ZielKordY = 0
+ZahlKord = []
+i = 0
+while i < LabHeight:
+    LabZeichen = []
+    j = 0
+    while j < LabLength:
+
+        if LabListeZeilen[i][j] == "W" and LabWall == "W":
+            LabZeichen.append(["WX",0,0])
+        elif LabListeZeilen[i][j] == "X":
+            LabZeichen.append(["WX", 0, 0])
+
+        elif LabListeZeilen[i][j] == "W" and LabWall == "X":
+            LabZeichen.append(["GX",0,0])
+        elif LabListeZeilen[i][j] == ".":
+            LabZeichen.append(["GX",0,0])
+        elif LabListeZeilen[i][j].isdigit():
+            LabZeichen.append(["GX", 0, 1])
+            ZahlKord.append([LabListeZeilen[i][j],i,j])
+
+        elif LabListeZeilen[i][j] == "S":
+            LabZeichen.append(["SX",1,0])
+        elif LabListeZeilen[i][j] == "Z":
+            LabZeichen.append(["ZX",0,0])
+        j = j + 1
+    LabNormal.append(LabZeichen)
+    i = i + 1
+
+
+print(LabListeZeilen)
+print(LabNormal)
+print(LabNormal[LabHeight-1][LabLength-1][0])
