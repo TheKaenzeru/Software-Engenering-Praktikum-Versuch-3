eaa630fe151d564beac44707dd16ff14b8b79490
Lemmy <lemmy_paul.schreiber@mailbox.tu-dresden.de>
2026-01-16 11:12:18 +0100

    test von Lab 3

--- DateiEinlesen.py
+++ DateiEinlesen.py
@@ -1,11 +1,17 @@
 import math
 import json
-with open("Labyrinth-1.txt", "r") as Labyrinth:
+with open("Labyrinth-3.txt", "r") as Labyrinth:
     LabHeight = 0
     LabLength = len(Labyrinth.readline()) - 1
+    LabWall = ""
     LabListeZeilen = []
     LabListeZeichen = []
-
+    Labyrinth.readline()
+    Labyrinth.readline()
+    if Labyrinth.read(1) == "W":
+        LabWall = "W"
+    else:
+        LabWall = "X"
 
     Labyrinth.seek(0)
     for line in Labyrinth:
@@ -15,5 +21,6 @@ with open("Labyrinth-1.txt", "r") as Labyrinth:
     print(LabListeZeilen)
     print(LabHeight)
     print(LabLength)
+    print(LabWall)
 
     Labyrinth.close()
