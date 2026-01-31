09454fbc4135ebf8103c59421da7bfe32215ebbf
ameliebremer <amelie.bremer06@icloud.com>
2026-01-31 14:01:11 +0100

    meine Programme ohne verbindung zu dem rest

--- LabyrinthZeichnen.py
+++ LabyrinthZeichnen.py
@@ -1,1 +1,35 @@
-import math
+#amelie
+
+import datei_einlesen
+import gfx_stack as gfx
+import kartendaten_normalisieren
+
+
+
+def Spielfeld_Zeichnen(Labyrinth):
+    höhe = len(Labyrinth) #Höhe ist die Länge der Liste, also die Anzahl der Strings
+    breite = len(Labyrinth[0]) #Breite ist die Länge von jedem String
+
+    gfx.init_once((breite, höhe)) #gibt das Ausgabefeld aus
+    for i in range(höhe):#kontrolliert jede Zeile
+        for j in range (breite):#kontrolliert jedes Element in der Zeile
+
+            if Labyrinth[i][j] == '.':  #Punkt ist Weg
+                gfx.set_pixel((j,i),'Elf Green') # der noch nicht gegangene Weg ist türkis
+
+            elif Labyrinth[i][j] == 'WX': #W ist Wand
+                gfx.set_pixel((j,i),'Black')# schwarz ist die farbe der wände
+
+            elif Labyrinth[i][j] == 'SX': #S ist Start
+                gfx.set_pixel((j, i), 'Atlantis')#start ist balu
+
+            elif Labyrinth[i][j] == 'ZX': #ZX ist Ziel
+                gfx.set_pixel((j, i),"Christi" ) #ziel ist grün
+
+            else: #alles andere, Bonusaufgaben
+                gfx.set_pixel((j, i), "Mandy" ) # hellrot dür die zuastzpunkte
+
+Labyrinth = kartendaten_normalisieren.normierung(datei_einlesen.Öffnen()) #Müssen sagen welches Labyrinth er benutzen soll,  eingabe bereits in Karte einlesen erfolgt
+Spielfeld_Zeichnen(Labyrinth) #Programm wird einmal ausgeführt
+while not gfx.stop_prog: #solange das Programm nicht beendet werden soll
+    gfx.event_loop()# weitere eingaben können verarbeitet werden
--- start und ende finden.py
+++ start und ende finden.py
@@ -1,1 +1,21 @@
 import math
+#amelie
+
+import kartendaten_normalisieren
+
+def Start_und_ziel_f(Labyrinth):
+    höhe = len(Labyrinth) #Anzahl der "Zeilen" als Strings
+    breite = len(Labyrinth[0]) #Anzahl der Elemente einer "Zeile"
+
+    for i in range(höhe): ###Für jedes i einen schleifendurchlauf
+        for j in range(breite): #Für jedes Element von i wird Schleife durchgeführt.
+            if Labyrinth[i][j] == 'SX': #wenn X gefunden wird = start
+                start = (i,j) #i,j werden "start" zugeordnet
+            elif Labyrinth[i][j] == 'ZX': #Falls ein "Z" gefunden wird = ziel
+                ziel = (i,j) #i,j werden "ziel" zugeordnet
+            else:
+                continue #Falls  kein zeichen: nächste Zeile absuchen
+
+    return start,ziel #Wenn alle "Zeilen" durchgegangen sind, wird "start" und "ziel" zurückgegeben
+
+Labyrinth = kartendaten_normalisieren.Labyrinth # warum dort? Variable "Labyrinth" weisen wir die  neuen Daten
